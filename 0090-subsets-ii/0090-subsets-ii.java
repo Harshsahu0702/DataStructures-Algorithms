e50d93c0e3f8065bd2986888759005f771a2a08c
Harshsahu0702 <harshsahu10072006@gmail.com>
2026-07-28 01:43:42 +0530

    Time: 2 ms (99.7%), Space: 45 MB (60.85%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+
+        Arrays.sort(nums);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+
+        backtrack(0, nums, curr, ans);
+
+        return ans;
+    }
+
+    private void backtrack(int start, int[] nums, List<Integer> curr, List<List<Integer>> ans) {
+
+        ans.add(new ArrayList<>(curr));
+
+        for (int i = start; i < nums.length; i++) {
+
+            // Skip duplicate elements at the same recursion level
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            curr.add(nums[i]);
+
+            backtrack(i + 1, nums, curr, ans);
+
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
